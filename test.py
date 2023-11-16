7a97206bdd2db6be87c0047c2ebf65174ffe43ff
antoinehabis <antoine.habis@telecom-paristech.fr>
2023-11-16 18:23:58 +0100

    erase unwanted file

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='A test program.')
-
-parser.add_argument("-s", "--split", help="Prints the supplied argument.", type=str)
-
-args = parser.parse_args()
-
-print(args.split)
